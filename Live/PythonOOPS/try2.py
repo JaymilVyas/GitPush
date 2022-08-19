982683925c333c9e5601b48fbbe2ee3fc3c758e2
Jaymil Vyas <105883080+JaymilVyas@users.noreply.github.com>
2022-08-19 11:28:04 +0530

    Update!

--- Live/PythonOOPS/try2.py
+++
@@ -1,9 +0,0 @@
-class person2:
-    def __init__(self,name,surname,yob):
-        self._name2=name
-        self.__surname2=surname
-        self.yob2=yob
-
-jimmy=person2("Jaymil","Vyas",1989)
-print(jimmy._name2)
-print(jimmy._person2__surname2)

50f314c16e077a75c9a50f5ef1192e9732832e60
Jaymil Vyas <105883080+JaymilVyas@users.noreply.github.com>
2022-08-21 15:29:44 +0530

    Update!

--- Live/Live22MySQLDatabase.py
+++ Live/Live22MySQLDatabase.py
@@ -1,2 +1,11 @@
-import mysql.connector as connection
+import mysql.connector as conn
 
+mydb = conn.connect(host = "localhost", user = "root", passwd = "Jimmy@123")
+#print(mydb)
+cursor = mydb.cursor()
+#cursor.execute("create database autoconnect")
+cursor.execute("show databases")
+s = "create table autoconnect.AutoDetails(employee_id int(10), employee_name varchar(80), employee_mailid varchar(40), employee_salary int(8), employee_attendance int(3))"
+#cursor.execute(s)
+#print(cursor.fetchall())
+cursor.execute(s)
